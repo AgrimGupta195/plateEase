32c158560c24dc53444553e2921f73f77738ecb5
Agrim Gupta <agrimgupt195@gmail.com>
2025-05-21 00:31:41 +0530

    add cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const cors= require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./lib/db');
 const userAuthRoutes = require('./routes/userAuthRoutes');
 const itemRoutes = require('./routes/itemRoutes');
 dotenv.config();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use("/api/userAuth",userAuthRoutes);
